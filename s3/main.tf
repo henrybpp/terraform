45ad34a21fd310bc82625037d94f7d596eeac06c
henrybpp <henryfisi08@gmail.com>
2024-06-23 00:33:54 -0400

    s3 create terraform

---
+++ s3/main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.aws_region
+}
+
+########################
+# Bucket creation
+########################
+resource "aws_s3_bucket" "my_protected_bucket" {
+  bucket = var.bucket_name
+}
+
+##########################
+# Bucket private access
+##########################
+resource "aws_s3_bucket_acl" "my_protected_bucket_acl" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+#############################
+# Enable bucket versioning
+#############################
+resource "aws_s3_bucket_versioning" "my_protected_bucket_versioning" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Creating Lifecycle Rule
+############################
+resource "aws_s3_bucket_lifecycle_configuration" "my_protected_bucket_lifecycle_rule" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.my_protected_bucket_versioning]
+
+  bucket = aws_s3_bucket.my_protected_bucket.bucket
+
+  rule {
+    id = "basic_config"
+    status = "Enabled"
+
+    filter {
+      prefix = "config/"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 60
+      storage_class   = "GLACIER"
+    }
+    
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
+}
+
+########################
+# Disabling bucket
+# public access
+########################
+resource "aws_s3_bucket_public_access_block" "my_protected_bucket_access" {
+  bucket = aws_s3_bucket.my_protected_bucket.id
+
+  # Block public access
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
